3f4156201bf76df6d3a0eaaa0e91482762930b49
potatodevsr <hoshiku1997@gmail.com>
2024-05-12 14:18:42 +0700

    12. Conditional Rendering in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,25 @@
 "use strict";
 
 console.log('App.js is running!');
-// create a app objact title/subtitle
 var app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer'
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
-
-// JSX -Javascript XML
-var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two")));
-
-// create a templateTwo var JSX expression
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options && app.options.length > 0 ? 'Here are you options' : 'No options'), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two")));
 var user = {
   name: 'Chadapohn',
   age: 26,
   location: 'Thailand'
 };
-// var userName = 'Chadapohn';
-// var userAge = 26;
-// var userLocation = 'Thailand';
-var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name), /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), /*#__PURE__*/React.createElement("p", null, "Location: ", user.location));
+function getLocation(location) {
+  console.log('location ->', location);
+  if (location) {
+    return /*#__PURE__*/React.createElement("p", null, "Location:", location);
+  } else {
+    return 'Unknown';
+  }
+}
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,35 +1,46 @@
 console.log('App.js is running!');
-// create a app objact title/subtitle
+
 var app = {
     title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer'
-}
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
 
-// JSX -Javascript XML
 var template =
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {/*  ถ้า app.subtitle มีค่าให้แสดง <p> ที่มีค่า subtitle ภายใน */}
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {/*  ถ้า app.options มีค่าและมีจำนวน options มากกว่า 0 ให้แสดงข้อความ 'Here are your options' แต่ถ้าไม่มีให้แสดงข้อความ 'No options' */}
+        <p>{app.options && app.options.length > 0 ? 'Here are you options' : 'No options'}</p>
         <ol>
             <li>Item one</li>
             <li>Item two</li>
         </ol>
-    </div>;
+    </div >;
 
-// create a templateTwo var JSX expression
 var user = {
     name: 'Chadapohn',
     age: 26,
     location: 'Thailand'
 }
-// var userName = 'Chadapohn';
-// var userAge = 26;
-// var userLocation = 'Thailand';
+
+function getLocation(location) {
+    console.log('location ->', location);
+    if (location) {
+        return <p>Location:{location}</p>;
+    } else {
+        return 'Unknown';
+    }
+}
+
 var templateTwo =
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        {/* ถ้า user.name มีค่า จะนำชื่อของ user มาแสดง; หากไม่มี, แสดงว่า 'Anonymous' */}
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {/* ถ้า user.age มีค่า และค่า age >= 18, แสดงข้อความ "Age: {user.age}" */}
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>;
 
 var appRoot = document.getElementById('app');
