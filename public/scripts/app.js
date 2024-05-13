65c252e06fc3c2b2a902df8d2d928c3d9148891b
potatodevsr <hoshiku1997@gmail.com>
2024-05-13 21:40:14 +0700

    14. ES6 Aside: Arrow Functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,25 +1,17 @@
 "use strict";
 
-console.log('App.js is running!');
-var app = {
-  title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
+console.log('es6-arrow-function.js');
+
+// function square(x) {
+//     return x * x;
+// };
+// console.log('square ->', square(3));
+
+// const squareArrow = (x) => {
+//     return x * x;
+// };
+
+var squareArrow = function squareArrow(x) {
+  return x * x;
 };
-var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options && app.options.length > 0 ? 'Here are you options' : 'No options'), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two")));
-var user = {
-  name: 'Chadapohn',
-  age: 26,
-  location: 'Thailand'
-};
-function getLocation(location) {
-  console.log('location ->', location);
-  if (location) {
-    return /*#__PURE__*/React.createElement("p", null, "Location:", location);
-  } else {
-    return 'Unknown';
-  }
-}
-var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+console.log('squareArrow ->', squareArrow(4));
---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,21 @@
+console.log('es6-arrow-function.js');
+
+// function square(x) {
+//     return x * x;
+// };
+// console.log('square ->', square(3));
+
+// const squareArrow = (x) => {
+//     return x * x;
+// };
+
+// const squareArrow = (x) => x * x;
+// console.log('squareArrow ->', squareArrow(4));
+// const getFirstName = (fullName) => {
+//     return fullName.split(' ')[0];
+// };
+
+const getFirstName = (fullName) => fullName.split(' ')[0];
+console.log(getFirstName('Chadapohn Sorakanit'))
+
+    ;
